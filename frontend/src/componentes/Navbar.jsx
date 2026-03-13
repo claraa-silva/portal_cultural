165d406e2b2ba97c10b467af7bb0c8705f7d1357
milena-del-moura <milena.moura@aluno.ifsp.edu.br>
2026-03-13 13:16:00 -0300

    navbar funcionando

--- frontend/src/componentes/Navbar.jsx
+++ frontend/src/componentes/Navbar.jsx
@@ -1,16 +1,19 @@
-function Navbar(){
+import React, { useState, useEffect } from "react";
+import '../App.css'
 
+export function Navbar(){
 
     return(
         <nav className="navbar">
-            <div className="logo">MeuSite</div>
-            
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
+            <div className="navbar-logo">
+                <a href="/">NossaLogo</a>
+            </div>
+            <ul className="navbar-links">
+                <li><a href="/">Início</a></li>
+                <li><a href="/servicos">Serviços</a></li>
                 <li><a href="/sobre">Sobre</a></li>
-                <li><a href="/contato">Contato</a></li>
+                <li><a href="/contato" className="btn-contato">Contato</a></li>
             </ul>
         </nav>
     )
 }
-export default Navbar;
